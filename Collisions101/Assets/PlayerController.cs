81ee7f8f2c016ce6f8fd74b154181419d3f6f1e0
Douglas Urner <dlu@canishe.com>
2018-01-23 22:50:49 -0800

    Very simple player collision & trigger detection.

---
+++ Collisions101/Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 1f;
+
+	private Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+		rb.AddForce(speed * Input.GetAxis("Horizontal"), 0, speed * Input.GetAxis("Vertical"));
+	}
+
+	private void OnCollisionEnter(Collision other) {
+		Debug.Log("OnCollisionEnter(): " + name + " collided with " + other.gameObject.name);
+		Destroy(other.gameObject);
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		Debug.Log("OnTriggerEnter(): " + name + " meets " + other.gameObject.name);
+		Destroy(other.gameObject);
+	}
+}
