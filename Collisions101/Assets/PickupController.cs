33c0e719d07ac0dc2dcc5a10d2316c95b6c1247a
Douglas Urner <dlu@canishe.com>
2018-01-23 23:07:01 -0800

    Add pickup controller.

---
+++ Collisions101/Assets/PickupController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupController : MonoBehaviour {
+	
+	private void OnDestroy() {
+		Debug.Log(name + ".OnDestory()");
+	}
+}
